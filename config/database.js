57fe8f7015ae0ce663ea3c224d3d5c50e81e2834
Renan Pereira <renanmol87@gmail.com>
2021-11-27 12:25:14 -0300

    add env folder

--- config/database.js
+++ config/database.js
@@ -1,18 +1,25 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'temperature'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'docker'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {}
+      options: {
+        ssl: true,
+      },
     },
   },
 });
